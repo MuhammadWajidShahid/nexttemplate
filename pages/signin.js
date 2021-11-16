904c8aace202c57c012b81e109fcbada2dbcf707
Muhammad Wajid <muhammad.wajid.shahid@gmail.com>
2021-11-16 19:33:16 +0500

    Fixed Useeffect bug

--- pages/signin.js
+++ pages/signin.js
@@ -1,65 +1,88 @@
-import React, { useState } from 'react'
-import api from '../src/api'
+import React, { useState } from "react";
+import api from "../src/api";
 import Router, { useRouter } from "next/router";
-import { Button, Container, TextField } from '@material-ui/core';
-import Redirect from '../src/redirect';
-import { makeStyles } from '@material-ui/core/styles'
+import { Button, Container, TextField } from "@material-ui/core";
+import Redirect from "../src/redirect";
+import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles({
   signIn: {
     display: "flex",
-    justifyContent: 'center',
+    justifyContent: "center",
     alignItems: "center",
     height: "70vh",
     gap: "10px",
-    flexDirection: 'column'
+    flexDirection: "column",
   },
   submitButton: {
-    placeSelf: 'flex-start',
-  }
+    placeSelf: "flex-start",
+  },
 });
 export default function SignIn() {
   const classes = useStyles();
-  const router = useRouter()
+  const router = useRouter();
   const { cb } = router.query;
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
-    email: "bruno@email.com", password: "bruno"
-  })
+    email: "bruno@email.com",
+    password: "bruno",
+  });
   const HandleSumbit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       setLoading(true);
-      const res = await api.signIn(null, "/auth/login", formData)
+      const res = await api.signIn(null, "/auth/login", formData);
       if (res?.statusText === "OK") {
         Router.push(cb || "/");
       }
       setLoading(false);
-    }
-    catch (err) {
-      if (err.response.status === 401)
-        setError(true);
+    } catch (err) {
+      if (err?.response?.status === 401) setError(true);
       setLoading(false);
     }
-  }
+  };
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
+  };
   return (
-    <Container maxWidth="sm" >
-      <form onSubmit={HandleSumbit} onChange={handleChange} className={classes.signIn}>
+    <Container maxWidth="sm">
+      <form
+        onSubmit={HandleSumbit}
+        onChange={handleChange}
+        className={classes.signIn}
+      >
         <h1>Sign In</h1>
-        {
-          error && <div style={{ color: "red" }}>
-            Email or Password is incorrect
-  </div>
-        }
-        <TextField fullWidth label="Email" type="email" name="email" value={formData.email} name="email" />
-        <TextField fullWidth label="Password" type="password" name="password" value={formData.password} name="password" />
-        <Button type="submit" disabled={loading} variant="contained" color="primary" className={classes.submitButton}>Sign In</Button>
+        {error && (
+          <div style={{ color: "red" }}>Email or Password is incorrect</div>
+        )}
+        <TextField
+          fullWidth
+          label="Email"
+          type="email"
+          name="email"
+          value={formData.email}
+          name="email"
+        />
+        <TextField
+          fullWidth
+          label="Password"
+          type="password"
+          name="password"
+          value={formData.password}
+          name="password"
+        />
+        <Button
+          type="submit"
+          disabled={loading}
+          variant="contained"
+          color="primary"
+          className={classes.submitButton}
+        >
+          Sign In
+        </Button>
       </form>
-    </Container >
-  )
+    </Container>
+  );
 }
 
 SignIn.getInitialProps = async (context) => {
@@ -67,25 +90,26 @@ SignIn.getInitialProps = async (context) => {
   if (user) {
     let cb = "/";
     if (context && context.req && context.req?.query?.cb) {
-      cb = req.query.cb
+      cb = req.query.cb;
     }
     if (typeof window !== "undefined") {
       function parseQuery(queryString) {
         var query = {};
-        var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+        var pairs = (
+          queryString[0] === "?" ? queryString.substr(1) : queryString
+        ).split("&");
         for (var i = 0; i < pairs.length; i++) {
-          var pair = pairs[i].split('=');
-          query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+          var pair = pairs[i].split("=");
+          query[decodeURIComponent(pair[0])] = decodeURIComponent(
+            pair[1] || ""
+          );
         }
         return query;
       }
       const parsed = parseQuery(window.location.search).cb;
-      if (parsed)
-        cb = parsed;
+      if (parsed) cb = parsed;
     }
-    Redirect(context, cb)
+    Redirect(context, cb);
   }
-  return {
-
-  }
-}
+  return {};
+};
--- src/useAuth/client.js
+++ src/useAuth/client.js
@@ -1,18 +1,17 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import api from "../api";
 
-
 export default function getUser(context) {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        (async ()=>{
-            // I am using /product route for just test purposes. You should add your own route which will return a user data
-            const res = await api.getUser(context, "/products")
-            setLoading(false);
-            setUser(res);
-        })()
-    })
-    return [user, loading];
+  useEffect(() => {
+    (async () => {
+      // I am using /product route for just test purposes. You should add your own route which will return a user data
+      const res = await api.getUser(context, "/products");
+      setLoading(false);
+      setUser(res);
+    })();
+  }, [api]);
+  return [user, loading];
 }
